057254dd1737be0292899c6b713ddf1ac7a08e92
Aviv <aviv.dror@gmail.com>
2024-05-21 22:51:24 +0200

    refactor

--- 07-api-calls/pokemon/exercises/pokemon-with-ability.js
+++ 07-api-calls/pokemon/exercises/pokemon-with-ability.js
@@ -1,4 +1,5 @@
 import { ORIGIN } from '../config.js';
+import XMLHttpRequest from 'xhr2';
 
 /**
  * Returns an array of all Pokemon with a specific ability.
@@ -10,30 +11,25 @@ import { ORIGIN } from '../config.js';
  * @throws {Error} HTTP error! status: {number}.
  */
 export const pokemonWithAbility = async (ability = '') => {
-    // --- generate and declare your resource's URL ---
-    // docs: https://pokeapi.co/docs/v2#abilities
-    const URL = _;
-
-    // --- fetch the API data (this works!) ---
-    const encodedURL = encodeURI(URL);
-    const response = await fetch(encodedURL);
-
-    // --- throw an error if the response is not ok (this works!) ---
-    if (!response.ok) {
-        const message = response.statusText
-            ? `${response.status}: ${response.statusText}\n-> ${URL}`
-            : `HTTP error! status: ${response.status}\n-> ${URL}`;
+    const URL = `${ORIGIN}/ability/${ability}`;
+    const reportError = (error) => {
+        const message = error.statusText
+            ? `${error.status}: ${error.statusText}\n-> ${URL}`
+            : `HTTP error! status: ${error.status}\n-> ${URL}`;
         throw new Error(message);
-    }
-
-    /* --- parse the data if the response was ok (this works!) ---*/
-    const data = await response.json();
-
-    // --- process the fetched data (if necessary) ---
-    //  you do not need to use `await` below this comment
-    //  you can refactor this to a separate logic function and test it
-    const pokemon = _;
-
-    // --- return the final data ---
+    };
+    const wrapper = async () => {
+        return new Promise((resolve, reject) => {
+            let xReq = new XMLHttpRequest();
+            xReq.open('GET', URL, true);
+            xReq.onload = (event) => {
+                resolve(JSON.parse(xReq.response));
+            };
+            xReq.onerror = reportError;
+            xReq.send();
+        });
+    };
+    const result = await wrapper();
+    const pokemon = result.pokemon.map((p) => p.pokemon);
     return pokemon;
 };

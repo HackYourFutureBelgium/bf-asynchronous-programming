26fb79f009947d196c9c31bcd3998e572506a68b
Aviv <aviv.dror@gmail.com>
2024-06-02 01:51:21 +0200

    remove unused logic

---
+++ 09-reverse-engineer/2-github-finder/src/init.js
@@ -0,0 +1,8 @@
+// https://api.potterdb.com/v1/characters?page[number]=1&page[size]=8&filter[name_cont]=ass&sort=gender desc
+// https://api.potterdb.com/v1/spells?page[number]=1&page[size]=80&sort=effect&filter[effect_cont]=as
+// https://members-api.parliament.uk/api/Members/Search?skip=2
+// https://web.dragonball-api.com
+// https://docs.potterdb.com/apis/rest
+// https://api.potterdb.com/v1/characters
+// https://api.artic.edu/api/v1/artworks/search?q=ta&from=2&size=8&fields=title&sort[id]=asc
+// https://openlibrary.org/search.json?title=as&limit=4&offset=4&sort=rating%20desc&fields=author_name
